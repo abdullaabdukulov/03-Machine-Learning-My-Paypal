9181012d6647a6600f3e5675a37a1abdac82b189
user <user@MacBook-Air--Yagoprogrammer.local>
2024-03-06 23:23:29 +0500

    finish3

--- py_compress.py
+++ py_compress.py
@@ -1,6 +1,7 @@
 import os
 from ipynbcompress import compress
+
 filename = './my_paypal.ipynb'
 out = './compressed_my_paypal.ipynb'
-os.stat(filename).st_size
-compress(filename, output_filename=out, img_width=800, img_format='jpeg')
+
+compress(filename, output_filename=out, img_width=500, img_format='png')
